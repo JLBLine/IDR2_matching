c3b01a6fd98fa4c8f8875e29bd298a9e84fa1798
Jack Line <jack.l.b.line@gmail.com>
2016-01-14 11:11:30 +1100

    Add the files for multifreq folder minus the large text files

---
+++ multifreq/use_choices.sh
@@ -0,0 +1,6 @@
+python use_choices.py --matched_cats=gleam_multi,vlssr,mrc,sumss,nvss,atg20 \
+	--pref_cats=nvss,sumss,gleam_multi,mrc,vlssr,atg20 \
+	--cat_freqs=76~84~92~99~107~115~122~130~143~151~158~166~174~181~189~197~204~212~220~227,74,408,843,1400,5000.0~8000.0~20000.0 \
+	--input_bayes=puma_gleammulti-v-m-s-n-a-eyeball.txt \
+	--decisions=test_choices.txt \
+	--tag=eyeball
